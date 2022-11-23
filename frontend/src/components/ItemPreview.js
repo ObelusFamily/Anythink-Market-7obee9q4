0d5cc3ba7434257e7c461c4ceaa2c0c35d50ffba
AndresBoni <andresboni91@gmail.com>
2022-11-22 22:43:36 -0300

    user image placeholder

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -56,6 +56,11 @@ const ItemPreview = (props) => {
               src={item.seller.image}
               alt={item.seller.username}
               className="user-pic rounded-circle pr-1"
+              onError={({ currentTarget }) => {
+                currentTarget.onerror = null;
+                currentTarget.src="../placeholder.png";
+              }}
+              placeholder={"../placeholder.png"}
             />
           </Link>
           <button className="btn btn-outline-secondary" onClick={handleClick}>
